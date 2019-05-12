7f544f2e7b0e15d81d8fc427bc9c7b2613a741ee
swapnanasina <swapnan.java9@gmail.com>
2019-05-12 15:52:09 -0700

    fourth commit

---
+++ Calculator1/src/com/calculator/ws/Calculator2.java
@@ -0,0 +1,16 @@
+package com.calculator.ws;
+
+public class Calculator2 {
+
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+	
+	public int substract(int a,int b) {
+		return a-b;
+	
+	
+	
+	}
+}
