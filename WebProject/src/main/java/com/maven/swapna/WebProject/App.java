314abdb191eda5c76e6f9202df85bacf7ad6e08b
swapnanasina <swapnan.java9@gmail.com>
2019-05-12 16:51:04 -0700

    First Commit

---
+++ WebProject/src/main/java/com/maven/swapna/WebProject/App.java
@@ -0,0 +1,13 @@
+package com.maven.swapna.WebProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
