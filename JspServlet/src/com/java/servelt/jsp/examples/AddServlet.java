d035f16f60b65d5d9ba967da95af24d431f08ac1
swapnanasina <swapnan.java9@gmail.com>
2019-05-13 19:13:50 -0700

    First Commit

---
+++ JspServlet/src/com/java/servelt/jsp/examples/AddServlet.java
@@ -0,0 +1,27 @@
+package com.java.servelt.jsp.examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddServlet
+ */
+@WebServlet("/add")
+public class AddServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+
+		int k = i + j;
+
+		PrintWriter out = res.getWriter();
+
+		out.print("output:" + k);
+	}
+}
